b6a39be149423f84a4ab9da5a5d80f1715fd1bc0
jordila <jordila@librebits.info>
2018-08-10 11:17:25 +0200

    ++ about.blade ++ home.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,8 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #bbb;
+                color: #e36b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
