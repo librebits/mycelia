315e1bb8a013a1bd89e7b4ae3eae1ff25f499f3e
jordila <jordila@librebits.info>
2018-08-21 14:19:29 +0200

    ++ Header Jumbotron @ layout ii @if '/'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,9 @@
   <body>
       @include('inc.navbar')
     <div class="container">
+      @if(Request::is('/'))
             @include('inc.showcase')
+      @endif
       <div class="row">
 
         <div class="col-md-8 col-lg-8">
