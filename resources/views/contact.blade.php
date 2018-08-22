514d19fc4e8752c57d525d63b3e6c1a85aa6e4a4
jordila <jordila@librebits.info>
2018-08-22 15:40:53 +0200

    ++ form i

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,6 +7,26 @@
 
   <h1>Contact</h1>
 
+  {!! Form::open(['url' => 'contact/submit']) !!}
+
+
+    <div class="form-group">
+
+      {{Form::label('name', 'Name')}}
+      {{Form::text('name', ' Subcomandante Marcos')}}
+
+    </div>
+
+
+    <div class="form-group">
+
+      {{Form::label('email', 'E-Mail Address')}}
+      {{Form::text('email', 'elzn@riseup.net')}}
+
+    </div>
+
+  {!! Form::close() !!}
+
 @endsection
 
 
